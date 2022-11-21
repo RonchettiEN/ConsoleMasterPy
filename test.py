3b8c498d694a979da4c207ec122887b9c72d2cf1
Nico Ronchetti <53882417+RonchettiEN@users.noreply.github.com>
2022-11-21 16:35:45 -0300

    First Commit

---
+++ test.py
@@ -0,0 +1,9 @@
+import lib.ConsoleMasterPy as cmpy
+
+cmpy.change_windows_title("Test ._.")
+cmpy.hide_cursor()
+cmpy.change_windows_size(20, 5)
+cmpy.change_pixel_size(25)
+cmpy.go_xy(1, 1)
+cmpy.print_with_color([215, 211, 4], [105, 205, 101])
+cmpy.pause()
