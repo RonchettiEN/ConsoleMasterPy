42593a68a235f305b7b5a70ab2b011a39884140a
Nico Ronchetti <53882417+RonchettiEN@users.noreply.github.com>
2023-11-11 16:44:14 -0300

    new md and re-structure

--- game_of_life_example.py
+++ game_of_life_example.py
@@ -2,70 +2,67 @@ from src.ConsoleMasterPy import ConsoleMaster, exception_handler
 import random
 from time import sleep
 
-
 def copy_matrix(matrix):
     return [row[:] for row in matrix]
 
-
-w, h = 35, 35
-cm = ConsoleMaster("GAME OF LIFE", 13, w, h)
-
+width, height = 35, 35
+console_manager = ConsoleMaster("GAME OF LIFE", 13, width, height)
 
 @exception_handler
 def main():
+    generation_counter = 0
+    
+    while True:
+        if generation_counter == 0:
+            current_matrix = [[0 for x in range(width)] for y in range(height)]
+            next_matrix = copy_matrix(current_matrix)
+            cell_color = [random.randint(0, 255) for _ in range(3)]
 
-    counter = 0
-    while 1:
-        if counter == 0:
-            pass_matrix = [[0 for x in range(w)] for y in range(h)]
-            now_matrix = copy_matrix(pass_matrix)
-            colour = [random.randint(0, 255) for i in range(3)]
+            live_cell_rules = [random.randint(0, 1) for _ in range(9)]
+            birth_rules = [random.randint(0, 1) for _ in range(9)]
 
-            live_laws = [random.randint(0, 1) for i in range(9)]
-            born_laws = [random.randint(0, 1) for i in range(9)]
+            for x in range(width):
+                next_matrix[x][0] = 0
+                next_matrix[x][height - 1] = 0
+                next_matrix[x][1] = 1
+                next_matrix[x][height - 2] = 1
 
-            for x in range(w):
-                now_matrix[x][0] = 0
-                now_matrix[x][h - 1] = 0
-                now_matrix[x][1] = 1
-                now_matrix[x][h - 2] = 1
+            for y in range(height):
+                next_matrix[0][y] = 0
+                next_matrix[width - 1][y] = 0
+                next_matrix[1][y] = 1
+                next_matrix[width - 2][y] = 1
 
-            for y in range(h):
-                now_matrix[0][y] = 0
-                now_matrix[w - 1][y] = 0
-                now_matrix[1][y] = 1
-                now_matrix[w - 2][y] = 1
+            for x in range(1, width - 1):
+                for y in range(1, height - 1):
+                    cell_value = next_matrix[x][y]
+                    console_manager.go_xy(x, y)
+                    color = cell_color if cell_value else [0, 0, 0]
+                    console_manager.print_with_color(color, [0, 0, 0], " ")
+            generation_counter = 300
 
-            for x in range(1, w - 1):
-                for y in range(1, h - 1):
-                    value = now_matrix[x][y]
-                    cm.go_xy(x, y)
-                    color = colour if value else [0, 0, 0]
-                    cm.print_with_color(color, [0, 0, 0], " ")
-            counter = 300
-        counter -= 1
+        generation_counter -= 1
 
-        pass_matrix = copy_matrix(now_matrix)
-        for x in range(1, w - 1):
-            for y in range(1, h - 1):
+        current_matrix = copy_matrix(next_matrix)
+        for x in range(1, width - 1):
+            for y in range(1, height - 1):
                 neighbors = [(x + i, y + j) for i in range(-1, 2) for j in range(-1, 2) if not (i == 0 and j == 0)]
-                count = sum(1 for i, j in neighbors if 0 <= i <= w and 0 <= j <= h and pass_matrix[i][j] == 1)
+                count = sum(1 for i, j in neighbors if 0 <= i < width and 0 <= j < height and current_matrix[i][j] == 1)
 
-                if pass_matrix[x][y] == 1:
-                    if not live_laws[count]:
-                        now_matrix[x][y] = 0
-                        cm.go_xy(x, y)
-                        cm.print_with_color([0, 0, 0], [0, 0, 0], " ")
+                if current_matrix[x][y] == 1:
+                    if not live_cell_rules[count]:
+                        next_matrix[x][y] = 0
+                        console_manager.go_xy(x, y)
+                        console_manager.print_with_color([0, 0, 0], [0, 0, 0], " ")
                     else:
-                        now_matrix[x][y] = 1
+                        next_matrix[x][y] = 1
                 else:
-                    if born_laws[count]:
-                        now_matrix[x][y] = 1
-                        cm.go_xy(x, y)
-                        cm.print_with_color(colour, [0, 0, 0], " ")
+                    if birth_rules[count]:
+                        next_matrix[x][y] = 1
+                        console_manager.go_xy(x, y)
+                        console_manager.print_with_color(cell_color, [0, 0, 0], " ")
                     else:
-                        now_matrix[x][y] = 0
+                        next_matrix[x][y] = 0
         sleep(0.05)
-
 
 main()
--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 import setuptools
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name="ConsoleMasterPy",
-    version="1.0.4",
+    version="1.0.7.1",
     author="Ronchetti Ezequiel Nicolás",
     author_email="RonchettiEzequielNicolas@hotmail.com",
     description="Console/Terminal functionality",
-    long_description="Console/Terminal functionality",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -15,4 +19,8 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
+    install_requires=[
+        'cursor>=1.3.5',
+        'sty>=1.0.4',
+    ],
 )
